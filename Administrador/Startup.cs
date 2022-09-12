5ab981430cc3b86d74ac0549c9dab33938ff8760
Marco Antonio Cantero C <marcoant.27c@gmail.com>
2022-09-12 17:43:39 -0500

    Vista Login Inicio Administrador

--- Administrador/Startup.cs
+++ Administrador/Startup.cs
@@ -55,7 +55,7 @@ namespace Administrador
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Acceso}/{action=Index}/{id?}");
             });
 
             
